27b4e19aea7557d6547163e1f2d46c0cc0a027f5
Manu-Z <manuela.aristizabalz@uqvirtual.edu.co>
2024-09-22 08:51:14 -0500

    commit 1

--- co.edu.uniquindio.billeteraVirtualFx/src/main/java/co/edu/uniquindio/billeteravirtualfx/billeteravirtualfx/UsuarioApplication.java
+++ co.edu.uniquindio.billeteraVirtualFx/src/main/java/co/edu/uniquindio/billeteravirtualfx/billeteravirtualfx/UsuarioApplication.java
@@ -1,21 +1,42 @@
 package co.edu.uniquindio.billeteravirtualfx.billeteravirtualfx;
 
+import co.edu.uniquindio.billeteravirtualfx.billeteravirtualfx.ViewController.UsuarioViewController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class UsuarioApplication extends Application {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(UsuarioApplication.class.getResource("Usuario.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Usuario");
-        stage.setScene(scene);
-        stage.show();
+        this.primaryStage = stage;
+        this.primaryStage.setTitle("Billetera Virtual");
+        mostrarVentanaPrincipal();
+    }
+
+    public void mostrarVentanaPrincipal() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(UsuarioApplication.class.getResource("Usuario.fxml" +
+                    ""));
+            AnchorPane rootLayout = (AnchorPane) loader.load();
+            UsuarioViewController usuarioViewController = loader.getController();
+//            usuarioViewController.setAplicacion(this);
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            //scene.getStylesheets().add(getClass().getResource("estilos.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
